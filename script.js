1654e928c05e0c0337f3d09975f54da7088421a4
Omar Khaled <omaroun35@gmail.com>
2023-07-11 13:39:27 +0300

    Added the functionality for creating the card and the grid. As well as getting the book from the form.

--- script.js
+++ script.js
@@ -13,7 +13,9 @@ class Library {
     }
 
     addBook(newBook) {
-        this.books.push(newBook);
+        if (!this.doesBookExist(newBook)) {
+            this.books.push(newBook);
+        }
     }
 
     removeBook(toBeRemovedBook) {
@@ -31,6 +33,8 @@ class Library {
 
 const library = new Library();
 
+
+// Dom Elements:
 const addBookBtn = document.getElementById("addBookBtn")
     , booksGrid = document.getElementById("booksGrid")
     , modalDiv = document.getElementById("modal")
@@ -39,18 +43,24 @@ const addBookBtn = document.getElementById("addBookBtn")
     , errorMsg = document.getElementById("errorMsg")
     , submitBtn = document.getElementById("submit");
 
+    
+const resetForm = function() {
+    bookForm.reset();
+};
+
 const showModal = function() {
+    resetForm();
     blurLayer.classList.add("active");
     modalDiv.classList.add("active");
 };
 
 const hideModal = function() {
     blurLayer.classList.remove("active");
-    blurLayer.classList.remove("active");
+    modalDiv.classList.remove("active");
 };
 
-const resetForm = function() {
-    bookForm.reset();
+const resetGrid = function() {
+    booksGrid.innerHTML = "";
 };
 
 const getBookFromForm = function() {
@@ -59,9 +69,75 @@ const getBookFromForm = function() {
         , pages = document.getElementById("pages").value
         , isRead = document.getElementById("isRead").checked;
     
-    const newBook = new Book(title, author, pages, isRead);
+    return new Book(title, author, pages, isRead);
+};
 
+const addBookToLibrary = function(e) {
+
+    e.preventDefault();
+    const newBook = getBookFromForm();
+
+    if (library.doesBookExist(newBook)) {
+        errorMsg.textContent= "This book already exists in your library.";
+        errorMsg.classList.add("active");
+        return;
+    }
     library.addBook(newBook);
-    resetForm();
+    createGrid();
     hideModal();
 };
+
+const createBookCard = function(book) {
+    const bookCard = document.createElement("div")
+            , title = document.createElement("p")
+            , author = document.createElement("p")
+            , pages = document.createElement("p")
+            , buttons = document.createElement("div")
+            , read = document.createElement("button")
+            , remove = document.createElement("button");
+        
+        bookCard.classList.add("book-card");
+        buttons.classList.add("buttons");
+        remove.classList.add("remove");
+
+        if (book.isRead) {
+            if (read.classList.contains("not-read")) {
+                read.classList.remove("not-read");
+                read.textContent= "";
+            }
+            read.classList.add("read");
+            read.textContent= "Read";
+        } else {
+            if (read.classList.contains("read")) {
+                read.classList.remove("read")
+                read.textContent= "";
+            }
+            read.classList.add("not-read");
+            read.textContent= "Not Read";
+        }
+
+        title.textContent = book.title;
+        author.textContent = book.author;
+        pages.textContent = `${book.pages} pages`;
+        remove.textContent = "Remove";
+
+
+        bookCard.appendChild(title);
+        bookCard.appendChild(author);
+        bookCard.appendChild(pages);
+        buttons.appendChild(read);
+        buttons.appendChild(remove);
+        bookCard.appendChild(buttons);
+        booksGrid.appendChild(bookCard);
+
+}
+
+const createGrid = function() {
+    resetGrid();
+    for(let book of library.books) {
+        createBookCard(book);
+    }
+}
+
+addBookBtn.addEventListener("click", showModal);
+bookForm.addEventListener("submit", addBookToLibrary);
